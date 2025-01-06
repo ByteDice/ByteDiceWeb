7b0a14171fec8794b683734ba0e7f62e94cb44dd
ByteDice <bytedice.official@gmail.com>
2025-01-06 15:37:10 +0100

    why is this file in the git add list

--- pxDensity.js
+++ pxDensity.js
@@ -5,7 +5,6 @@ function calcPixelDensity() {
   let densityWidth =  document.documentElement.clientWidth / 256
   let densityHeight = document.documentElement.clientHeight / 164
 
-
   document.documentElement.style.setProperty("--pxDensity", Math.max(densityWidth, densityHeight))
   document.documentElement.style.setProperty("--pxDensityPx", `${Math.max(densityWidth, densityHeight)}px`)
 
