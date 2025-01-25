1fffee4958b9aef523222227118d216150dae71a
Byte Dice <bytedice.official@gmail.com>
2025-01-25 17:51:27 +0100

    added a 600x400 hidden license for when HTML->PNG conversion is added

--- toys/license/script.js
+++ toys/license/script.js
@@ -7,35 +7,39 @@ const inputOutputMap = {
   expireInput: "expireValue",
   identityInput: "genderValue",
   signatureInput: "signatureValue",
-};
+}
 
 for (const [inputId, outputId] of Object.entries(inputOutputMap)) {
-  const inputElement = document.getElementById(inputId);
-  const outputElement = document.getElementById(outputId);
+  const inputElement = document.getElementById(inputId)
+  const outputElement = document.getElementById(outputId)
+  const outputElement_600x400 = document.getElementById(`${outputId}_600x400`)
 
   if (inputElement && outputElement) {
     inputElement.oninput = function () {
-      outputElement.innerHTML = this.value;
-    };
+      outputElement.innerHTML = this.value
+      outputElement_600x400.innerHTML = this.value
+    }
   }
 }
 
-const sillyInput = document.getElementById("sillyInput");
-const sillyValue = document.getElementById("sillyValue");
-const sillyNumberValue = document.getElementById("sillyNumberValue");
+const sillyInput = document.getElementById("sillyInput")
+const sillyValue = document.getElementById("sillyValue")
+const sillyValue_600x400 = document.getElementById("sillyValue_600x400")
+const sillyNumberValue = document.getElementById("sillyNumberValue")
 
 if (sillyInput && sillyValue && sillyNumberValue) {
   sillyInput.oninput = function () {
-    setSillyValue(this.value);
-  };
+    setSillyValue(this.value)
+  }
 }
 
 function setSillyValue(value) {
-  sillyNumberValue.innerHTML = prependZero(value);
-  sillyValue.style.width = `${value * 10 + 0.1}%`;
+  sillyNumberValue.innerHTML = prependZero(value)
+  sillyValue.style.width = `${value * 10 + 0.1}%`
+  sillyValue_600x400.style.width = `${value * 10 + 0.1}%`
 
-  let percent = value == 0 ? 0 : 100 / value;
-  sillyValue.style.backgroundImage = `
+  let percent = value == 0 ? 0 : 100 / value
+  let bgImg = `
     repeating-linear-gradient(
       to right, 
       #000000 0px, 
@@ -43,11 +47,14 @@ function setSillyValue(value) {
       transparent 3px, 
       transparent ${percent}%
     )
-  `;
+  `
+  sillyValue.style.backgroundImage = bgImg
+  sillyValue_600x400.style.backgroundImage = bgImg
 }
 
 const picInput = document.getElementById("picInput")
 const picValue = document.getElementById("picValue")
+const picValue_600x400 = document.getElementById("picValue_600x400")
 const picUpload = document.getElementById("picUpload")
 const customPicOption = document.getElementById("customPic")
 
@@ -55,12 +62,15 @@ function setPictureValue() {
   if (picInput.value != "custom") {
     let src = `/assets/boykissers/${picInput.value}.png`
     picValue.src = src
+    picValue_600x400.src = src
   }
   else {
     if (picUpload.files.length > 0) {
       const file = picUpload.files[0]
       const fileName = file.name
-      picValue.src = URL.createObjectURL(file)
+      let src = URL.createObjectURL(file)
+      picValue.src = src
+      picValue_600x400.src = src
       customPicOption.innerHTML = `Custom (${fileName})`
     }
     else {
@@ -74,21 +84,23 @@ picUpload.oninput = function() { setPictureValue() }
 picInput.oninput = function() { setPictureValue() }
 
 function prependZero(number) {
-  return number < 10 ? "0" + number : number;
+  return number < 10 ? "0" + number : number
 }
 
 function onLoadLicense() {
   for (const [inputId, outputId] of Object.entries(inputOutputMap)) {
-    const inputElement = document.getElementById(inputId);
-    const outputElement = document.getElementById(outputId);
+    const inputElement = document.getElementById(inputId)
+    const outputElement = document.getElementById(outputId)
+    const outputElement_600x400 = document.getElementById(`${outputId}_600x400`)
 
     if (inputElement && outputElement) {
-      outputElement.innerHTML = inputElement.value;
+      outputElement.innerHTML = inputElement.value
+      outputElement_600x400.innerHTML = inputElement.value
     }
   }
 
   if (sillyInput) {
-    setSillyValue(sillyInput.value);
+    setSillyValue(sillyInput.value)
   }
 
   if (picInput) {
